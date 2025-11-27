78a068060892722854ec23ba6b68d5f37faafa3b
Dipendra Kumar <kdipendra7777@gmail.com>
2025-11-27 16:07:47 +0000

    add new file

---
+++ Getting started with java/HelloWorld.java
@@ -0,0 +1,56 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.print("Hey I am Java");
+        
+        // more about hello world 
+        // this is for new line
+        // System.out.print("\n");
+        // System.out.print("hey java");
+        // System.out.println("hello java");
+
+
+        // println is for new line
+
+
+
+        // Lecture 6
+
+        // int a=23;
+        // a=a+1;
+        // System.out.println(a++);
+        // System.out.println(a);
+
+        // System.out.println(++a);
+
+        // int a=60, b=80;
+        // if (a++ >60 && b++ >80) {
+        //     System.out.println("inside if");
+        // }else{
+        //     System.out.println("inside else");
+        // }
+
+        // System.out.println("a"+a +"b"+b);
+
+        // System.out.println("hey man");
+
+        // int a=10, b=2;
+        // System.out.println(a&b);     // bitwise and
+        // System.out.println(a|b);     // bitwise or
+        // System.out.println(a^b);     // bitwise xor
+        // System.out.println(~a);      // bitwise not
+
+        // System.out.println(a<<1);       // left shift
+        // System.out.println(a>>1);       // right shift
+ 
+        // assignment operator
+
+        // a+=1;
+        // a-=1;
+        // a*=1;
+        // a/=1;
+        // a^=1;
+        // a<<=1;
+
+        System.out.println("java");
+    }
+}
